3a7d78c7b1b7ad0dcf7b7b1b199c7a591a6f7186
stefandy29 <55833817+stefandy29@users.noreply.github.com>
2024-02-17 13:24:40 +0700

    Update app.js

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req,res){
-	res.send('Salam Brother');
+	res.send('Test');
 })
 
 app.listen(80, () => console.log('express server started successfully.'));
